e123f029132fe477bec43ebc471dd466cd618d55
thimi-codes <thimansaweragodage46@gmail.com>
2026-04-24 00:00:11 +0530

    Configure JAX-RS application with base path /api/v1

---
+++ src/main/java/com/smartcampusapi/SmartCampusApplication.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampusapi;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application{
+    
+}
